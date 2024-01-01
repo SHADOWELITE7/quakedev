26c2037f78959993f77b8be00c6e47c1b70a357a
SHADOWELITE7 <jasper.david.rogers@gmail.com>
2024-01-01 15:55:20 -0500

    new map

--- compile.bat
+++ compile.bat
@@ -5,25 +5,25 @@ cd E:\My_Workspace\quakedev
 
 
 echo Copying Files...
-copy E:\My_Workspace\quakedev\working\arena2.map E:\My_Workspace\quakedev
+copy E:\My_Workspace\quakedev\working\shad_dm2.map E:\My_Workspace\quakedev
 
 
 echo Converting map...
 
 
 echo --------------QBSP--------------
-E:\My_Workspace\quakedev\tools\ericw-tools-v0.18.1-32-g6660c5f-win64\bin\qbsp.exe arena2
+E:\My_Workspace\quakedev\tools\ericw-tools-v0.18.1-32-g6660c5f-win64\bin\qbsp.exe shad_dm2
 
 echo --------------VIS---------------
-E:\My_Workspace\quakedev\tools\ericw-tools-v0.18.1-32-g6660c5f-win64\bin\vis.exe arena2
+E:\My_Workspace\quakedev\tools\ericw-tools-v0.18.1-32-g6660c5f-win64\bin\vis.exe shad_dm2
 
 echo -------------LIGHT--------------
-E:\My_Workspace\quakedev\tools\ericw-tools-v0.18.1-32-g6660c5f-win64\bin\light.exe arena2
+E:\My_Workspace\quakedev\tools\ericw-tools-v0.18.1-32-g6660c5f-win64\bin\light.exe shad_dm2
 
-copy arena2.bsp E:\My_Workspace\quakedev\id1\maps
-copy arena2.pts E:\My_Workspace\quakedev\id1\maps
-copy arena2.lit E:\My_Workspace\quakedev\id1\maps
+copy shad_dm2.bsp E:\My_Workspace\quakedev\id1\maps
+copy shad_dm2.pts E:\My_Workspace\quakedev\id1\maps
+copy shad_dm2.lit E:\My_Workspace\quakedev\id1\maps
 pause
 cd\
 cd E:\My_Workspace\quakedev
-ironwail  +map arena2
+ironwail  +map shad_dm2
